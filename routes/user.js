a458af319c281194a4066a4fb07e63ee273fc278
Trisha Banerjee <trishabanerjee232@gmail.com>
2026-02-26 19:09:20 +0530

    updating application

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ router.route("/signup")
 .post(wrapAsync(userController.signup));
 
 
-
+//***
 router.route("/login")
 .get(userController.loginFormRender)
 .post(saveRedirectUrl, passport.authenticate("local", {
